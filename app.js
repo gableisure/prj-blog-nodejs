06c9cca4d420eecfa4012de41bf25ca00ba0ebbb
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2021-01-28 23:10:39 -0300

    Implementação da listagem de postagens por categoria

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const session = require("express-session");
 const flash = require("connect-flash");
 require("./models/Postagem");
 const Postagem = mongoose.model("postagens");
+require("./models/Categoria");
+const Categoria = mongoose.model("categorias");
 
 const PORT = 8083;
 
@@ -70,6 +72,34 @@ app.get("/postagem/:slug", (req, res) => {
         req.flash("error_msg", "Houve um erro interno!");
         res.redirect("/");
     });
+});
+
+app.get("/categorias", (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render("categoria/index", { categorias: categorias });
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno ao listar as categorias");
+        res.redirect("/");
+    })
+});
+
+app.get("/categorias/:slug", (req, res) => {
+    Categoria.findOne({ slug: req.params.slug }).lean().then((categoria) => {
+        if (categoria) {
+            Postagem.find({ categoria: categoria._id }).lean().then((postagens) => {
+                res.render("categoria/postagens", { categoria: categoria, postagens: postagens });
+            }).catch((err) => {
+                req.flash("error_msg", "Houve um erro ao listar as postagens");
+                res.redirect("/");
+            })
+        } else {
+            req.flash("error_msg", "Esta categoria não existe");
+            res.redirect("/");
+        }
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno ao carregar a página dessa categoria");
+        res.redirect("/");
+    })
 });
 
 app.get("/404", (req, res) => {
